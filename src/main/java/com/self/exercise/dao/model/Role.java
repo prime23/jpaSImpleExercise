c5d640c975d6063383117124b18b75b8fb80b926
prime23 <danzaljohnson@gmail.com>
2017-02-05 22:14:58 -0700

    change application.properties to application.yaml added config to create an im memory h2 database based off console output change the value of strategy for id values

--- src/main/java/com/self/exercise/dao/model/Role.java
+++ src/main/java/com/self/exercise/dao/model/Role.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
 
--- src/main/java/com/self/exercise/dao/model/User.java
+++ src/main/java/com/self/exercise/dao/model/User.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name = "user_name")
     private String userName;
